142568cb1a30dd00658e2296ebca3f7db36ae484
Igor Nathan Lira <nathanlira15@gmail.com>
2026-07-23 01:21:07 -0300

    chore(di): register auth, profile and closet dependencies in Registry

--- src/presentation/di/Registry.ts
+++ src/presentation/di/Registry.ts
@@ -1,11 +1,31 @@
-import { FirebasePostRepository } from "../../infrastructure/repositories/FirebasePostRepository";
-import { GetFeedPostsUseCase } from "../../application/useCases/GetFeedPostsUseCase";
-import { CreatePostUseCase } from "../../application/useCases/CreatePostUseCase";
+import { FirebasePostRepository } from "@/infrastructure/repositories/FirebasePostRepository";
+import { FirebaseAuthRepository } from "@/infrastructure/repositories/FirebaseAuthRepository";
+import { FirebaseUserRepository } from "@/infrastructure/repositories/FirebaseUserRepository";
+import { FirebaseClosetRepository } from "@/infrastructure/repositories/FirebaseClosetRepository";
 
-// 1. Instanciamos a nossa infraestrutura concreta (O Adaptador)
-// Trabalhamos como um Singleton simples para não recriar a conexão a cada renderização
-const postRepository = new FirebasePostRepository();
+import { GetFeedPostsUseCase } from "@/application/useCases/GetFeedPostsUseCase";
+import { CreatePostUseCase } from "@/application/useCases/CreatePostUseCase";
+import { LoginWithEmailUseCase } from "@/application/useCases/LoginWithEmailUseCase";
+import { LoginWithGoogleUseCase } from "@/application/useCases/LoginWithGoogleUseCase";
+import { RegisterWithEmailUseCase } from "@/application/useCases/RegisterWithEmailUseCase";
+import { LogoutUseCase } from "@/application/useCases/LogoutUseCase";
+import { GetUserProfileUseCase } from "@/application/useCases/GetUserProfileUseCase";
+import { GetClosetItemsUseCase } from "@/application/useCases/GetClosetItemsUseCase";
 
-// 2. Injetamos o repositório nos Casos de Uso
+// Instâncias de Infraestrutura (Adapters)
+export const postRepository = new FirebasePostRepository();
+export const authRepository = new FirebaseAuthRepository();
+export const userRepository = new FirebaseUserRepository();
+export const closetRepository = new FirebaseClosetRepository();
+
+// Injeção de Dependência nos Use Cases
 export const getFeedPostsUseCase = new GetFeedPostsUseCase(postRepository);
 export const createPostUseCase = new CreatePostUseCase(postRepository);
+
+export const loginWithEmailUseCase = new LoginWithEmailUseCase(authRepository);
+export const loginWithGoogleUseCase = new LoginWithGoogleUseCase(authRepository);
+export const registerWithEmailUseCase = new RegisterWithEmailUseCase(authRepository);
+export const logoutUseCase = new LogoutUseCase(authRepository);
+
+export const getUserProfileUseCase = new GetUserProfileUseCase(userRepository);
+export const getClosetItemsUseCase = new GetClosetItemsUseCase(closetRepository);
